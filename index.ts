e72edbed9379e1730641f39c27d18c5734cfe0fd
Schafer Gray <schafergray@Schafers-MacBook-Pro.local>
2024-02-23 15:08:59 -0500

    Updating discount route.

--- index.ts
+++ index.ts
@@ -3,16 +3,20 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 app.use(cors({
     // origin: 'https://schafergray.github.io'
     origin: 'http://localhost:4200'
 }))
 app.use(bodyParser.json());
-app.get('/discounts', (req: any, res: any) => {
-  let basket = req.basket;
-  // Logic to scour the basket for items that match discounts
-  res.json(basket)
+app.post('/discounts', (req: any, res: any) => {
+  let basket = req.body;
+  let basketDiscount = Number((basket.total * 0.25).toFixed(2));
+  res.send({
+    basket: basket,
+    discount: basketDiscount,
+    statusCode: 200
+  });
 });
 
 app.listen(port, () => {
